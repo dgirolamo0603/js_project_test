94a0eb698e150dff6c1c2662c88d693c0b76ec68
Aleksey <dgirolamo777@gmail.com>
2024-08-07 14:05:05 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+var number =5;
+var string = "Привет мир";
+var sym = Symbol();
+var boolean =true;
+null;
+undefined;
+var obj = {};
+
+let persone = {
+    name: "John",
+    age:25,
+    isMarried:false
+};
+
+console.log(persone.name);
+console.log(persone["name"]);
+console.log(persone["age"]);
+let arr=[1,"Алексе", "Влад",45];
+console.log(arr[2]);
+//alert("Hello, world!");
+// let age = prompt("Crjkmrj вам лет");
+// console.log(typeof(arr));
+
+// let rest = +prompt("Сколько вам лет?");
+// console.log(rest+5);
+
+let incr =10;
+let decr = 10;
+let sec=89;
+console.log(++incr);
+console.log(--decr);
+console.log(typeof(decr))
+console.log(0/sec);
